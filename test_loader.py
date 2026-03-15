fd57b39ea7c978f47586e4a005e6d53d05c6b05a
abbhishekrajj <abbhishek.rajj@gmail.com>
2026-03-15 14:46:25 +0530

    Rag Pipeline files

--- test_loader.py
+++
@@ -1,9 +0,0 @@
-from app.rag.embeddings import EmbeddingManager
-
-embed = EmbeddingManager()
-
-text = ["Machine learning is a part of AI"]
-
-vec = embed.generate_embeddings(text)
-
-print(vec.shape)
